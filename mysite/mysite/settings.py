53feea84eb9c4200017bf5d61b0034507a6fa886
Madhan Rajan <madhan@Madhans-MacBook-Pro-1333.local>
2019-09-27 00:07:03 +0100

    Added IP address

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '_-==mx5#x8hrm!&t-awnz-k)=+yjb221k#uryz4ae$bx@)7yze'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['68.183.154.129']
 
 
 # Application definition
