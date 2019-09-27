c2e1839bcc78aee1c8339b97bd8fe0bfce626bdf
Madhan Rajan <madhan@Madhans-MacBook-Pro-1333.local>
2019-09-27 23:36:16 +0100

    set

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '_-==mx5#x8hrm!&t-awnz-k)=+yjb221k#uryz4ae$bx@)7yze'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['68.183.154.129', 'sharemy.life', 'www.sharemy.life']
 
 
 # Application definition
@@ -96,24 +96,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite',
+        'USER': 'madhan',
+        'PASSWORD': 'stayreal357',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'mysite',
-            'USER': 'madhan',
-            'PASSWORD': 'stayreal357',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 # Password validation
